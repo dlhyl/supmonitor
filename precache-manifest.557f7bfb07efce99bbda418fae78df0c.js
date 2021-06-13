7275c19964f77070651ccf67a4c225a9ac4a82f1
dlhyl <dlhylubomir@gmail.com>
2021-06-13 22:13:51 +0200

    Updates

---
+++ precache-manifest.557f7bfb07efce99bbda418fae78df0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb9d6e36402b0e2e8e6fa45ed70c815",
+    "url": "/supmonitor/index.html"
+  },
+  {
+    "revision": "9a18a4f916c281560647",
+    "url": "/supmonitor/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4b50a0232787fdcbdb45",
+    "url": "/supmonitor/static/css/main.880c6546.chunk.css"
+  },
+  {
+    "revision": "9a18a4f916c281560647",
+    "url": "/supmonitor/static/js/2.b52b57d3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/supmonitor/static/js/2.b52b57d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b50a0232787fdcbdb45",
+    "url": "/supmonitor/static/js/main.33026342.chunk.js"
+  },
+  {
+    "revision": "d8b2fc7c0d923bc5c129",
+    "url": "/supmonitor/static/js/runtime-main.aaf42f13.js"
+  }
+]);
